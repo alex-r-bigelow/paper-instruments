7eca1a8d9e01d568a129719e7ff860854a9eb15e
yasashiku <alex.r.bigelow@gmail.com>
2014-08-27 09:39:10 -0600

    Added path editor

--- scripts/common.js
+++ scripts/common.js
@@ -12,6 +12,112 @@ function Image(src,z) {
     self.zIndex = z;
 }
 
+function Handle(shape, x, y, hIndex) {
+    var self = this,
+        domHandle;
+    
+    self.hash = Handle.HASH;
+    Handle.ALL[self.hash] = self;
+    Handle.HASH += 1;
+
+    self.shape = shape;
+    self.x = x;
+    self.y = y;
+    self.hIndex = hIndex;
+    
+    // jQuery doesn't support creating SVG nodes...
+    domHandle = document.createElementNS('http://www.w3.org/2000/svg','circle');
+    domHandle.setAttribute('id', 'Handle' + self.hash);
+    document.getElementById('hotSpots').appendChild(domHandle);
+    // Now set the other attributes
+    jQuery("#Handle" + self.hash).attr({
+        cx : self.x,
+        cy : self.y,
+        r : 5
+    }).on('mousedown', function (event) {
+        self.drag(event);
+    });
+}
+Handle.ALL = {};
+Handle.HASH = 1;
+Handle.clearAll = function () {
+    var hash;
+    for (hash in Handle.ALL) {
+        if (Handle.ALL.hasOwnProperty(hash)) {
+            jQuery("#Handle" + hash).remove();
+        }
+    }
+    Handle.ALL = {};
+    Handle.HASH = 1;
+};
+
+Handle.prototype.drag = function (event) {
+    var self = this,
+        origin = {
+            X : event.clientX - self.x,
+            Y : event.clientY - self.y
+        },
+        move = function (event) {
+            event.preventDefault();
+            self.moveTo({
+                X : event.clientX - origin.X,
+                Y : event.clientY - origin.Y
+            });
+            return true;
+        },
+        up = function (event) {
+            document.removeEventListener('mousemove', move);
+            document.removeEventListener('mouseup', up);
+            return move(event);
+        };
+    
+    event.preventDefault();
+    
+    document.addEventListener('mousemove', move);
+    document.addEventListener('mouseup', up);
+    
+    return true;
+};
+Handle.prototype.moveTo = function (pos) {
+    var self = this,
+        segments = self.shape.extractSegments(),
+        s,
+        l,
+        hIndex;
+    self.x = pos.X;
+    self.y = pos.Y;
+    jQuery('#Handle' + self.hash).attr({
+        'cx' : self.x,
+        'cy' : self.y
+    });
+    
+    // Update the path string in our parent shape
+    hIndex = 0;
+    for (s = 0; s < segments.length; s += 1) {
+        if (hIndex === self.hIndex) {
+            segments[s].points[0].X = self.x;
+            segments[s].points[0].Y = self.y;
+            self.shape.setD(segments);
+            self.shape.finalizeD();
+            break;
+        } else {
+            hIndex += 1;
+        }
+        l = segments[s].segType.toLowerCase();
+        if (l === 's' || l === 'q') {
+            if (hIndex === self.hIndex) {
+                segments[s].points[1].X = self.x;
+                segments[s].points[1].Y = self.y;
+                self.shape.setD(segments);
+                self.shape.finalizeD();
+                break;
+            } else {
+                hIndex += 1;
+            }
+        }
+    }
+};
+
 function Shape(d,visible,zIndex) {
     var self = this;
     
@@ -36,9 +142,159 @@ function Shape(d,visible,zIndex) {
 }
 Shape.ALL = {};
 Shape.HASH = 1;
+Shape.SELECTED = null;
 Shape.findShape = function (domTarget) {
     var hash = Number(domTarget.getAttribute('id').substring(7));
     return Shape.ALL[hash];
+};
+Shape.prototype.extractSegments = function () {
+    var self = this,
+        numbers = self.d.replace(new RegExp("[a-zA-Z]", "g"), ",").split(","),
+        letterString = self.d.replace(new RegExp("[0-9]", "g"), ""),
+        letters = letterString.split(","),
+        lowerLetters = letterString.toLowerCase().split(","),
+        segments = [],
+        splicedNumbers,
+        l;
+    
+    // There's an empty entry at the beginning
+    numbers.splice(0, 1);
+    
+    for (l = 0; l < letters.length; l += 1) {
+        if (lowerLetters[l] === "z") {
+            segments.push({
+                segType : letters[l],
+                points : []
+            });
+        } else if (lowerLetters[l] === "m" || lowerLetters[l] === "l" || lowerLetters[l] === "t") {
+            splicedNumbers = numbers.splice(0, 2);
+            segments.push({
+                segType : letters[l],
+                points : [
+                    {
+                        X : splicedNumbers[0],
+                        Y : splicedNumbers[1]
+                    }
+                ]
+            });
+        } else if (lowerLetters[l] === "s" || lowerLetters[l] === "q") {
+            splicedNumbers = numbers.splice(0, 4);
+            segments.push({
+                segType : letters[l],
+                points : [
+                    {
+                        X : splicedNumbers[0],
+                        Y : splicedNumbers[1]
+                    },
+                    {
+                        X : splicedNumbers[2],
+                        Y : splicedNumbers[3]
+                    }
+                ]
+            });
+        }
+    }
+    return segments;
+};
+Shape.prototype.setD = function (segments) {
+    var self = this,
+        s,
+        l;
+    
+    self.d = "";
+    
+    for (s = 0; s < segments.length; s += 1) {
+        l = segments[s].segType.toLowerCase();
+        self.d += segments[s].segType;
+        if (l === 'z') {
+            break;
+        }
+        self.d += segments[s].points[0].X;
+        self.d += ',';
+        self.d += segments[s].points[0].Y;
+        if (l === 's' || l === 'q') {
+            self.d += ",";
+            self.d += segments[s].points[1].X;
+            self.d += ',';
+            self.d += segments[s].points[1].Y;
+        }
+    }
+};
+Shape.prototype.finalizeD = function () {
+    var self = this;
+    jQuery('#pathString').val(self.d);
+    jQuery('#HotSpot' + self.hash).attr({
+        'd' : self.d
+    });
+};
+Shape.prototype.initHandles = function () {
+    var self = this,
+        segments = self.extractSegments(),
+        i,
+        p,
+        h,
+        hIndex;
+    Handle.clearAll();
+    jQuery("#pathString").val(self.d);
+    hIndex = 0;
+    for (i = 0; i < segments.length; i += 1) {
+        for (p = 0; p < segments[i].points.length; p += 1) {
+            h = new Handle(self, segments[i].points[p].X, segments[i].points[p].Y, hIndex);
+            hIndex += 1;
+        }
+    }
+};
+Shape.prototype.select = function (event) {
+    var self = this,
+        domHandle,
+        hash,
+        origins = {},
+        newPos,
+        target,
+        move = function (event) {
+            var hash;
+            event.preventDefault();
+            
+            for (hash in Handle.ALL) {
+                if (Handle.ALL.hasOwnProperty(hash)) {
+                    Handle.ALL[hash].moveTo({
+                        X : event.clientX - origins[hash].X,
+                        Y : event.clientY - origins[hash].Y
+                    });
+                }
+            }
+            self.finalizeD();
+            
+            return true;
+        },
+        up = function (event) {
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+                
+                return move(event);
+        };
+    
+    Shape.SELECTED = self.hash;
+    
+    // Create our handles
+    self.initHandles();
+    
+    // Now drag the whole shape
+    event.preventDefault();
+    
+    for (hash in Handle.ALL) {
+        if (Handle.ALL.hasOwnProperty(hash)) {
+            origins[hash] = {
+                X : event.clientX - Handle.ALL[hash].x,
+                Y : event.clientY - Handle.ALL[hash].y
+            };
+        }
+    }
+    
+    document.addEventListener("mousemove", move);
+    document.addEventListener("mouseup", up);
+    
+    return true;
 };
 
 function DummyAction(mask) {
@@ -62,7 +318,6 @@ var no_image = new Image('',1),
     empty_space = new Shape('M0,0L512,0L512,343L0,343Z',true,0), // TODO: figure this out automatically
     config,
     metaActions,
-    differentiateMasks = false,
     currentComboString = null;
 
 // Function that handles the preview - by default this gets called whenever state changes,
@@ -139,8 +394,22 @@ function updatePreview(updateCallback) {
     previewHotSpots.enter().append("path");
     
     previewHotSpots.attr("d", function (d) { return d.hotSpot.d; })
-        .attr("id", function (d) { return "HotSpot" + d.hotSpot.hash; })
-        .each(function (d) {
+        .attr("id", function (d) { return "HotSpot" + d.hotSpot.hash; });
+    
+    if (Shape.SELECTED !== null) {
+        previewHotSpots.attr("class", function (d) {
+            if (d.isMask === true) {
+                return 'mask';
+            } else {
+                return "";
+            }})
+            .each(function (d) {
+                jQuery("#HotSpot" + d.hotSpot.hash).on('mousedown', function (event) {
+                    Shape.ALL[d.hotSpot.hash].select(event);
+                });
+            });
+    } else {
+        previewHotSpots.each(function (d) {
             var eventString;
             for (eventString in d.events) {
                 if (d.events.hasOwnProperty(eventString)) {
@@ -153,14 +422,6 @@ function updatePreview(updateCallback) {
                 }
             }
         });
-    
-    if (differentiateMasks === true) {
-        previewHotSpots.attr("class", function (d) {
-            if (d.isMask === true) {
-                return 'mask';
-            } else {
-                return "";
-            }});
     }
     
     previewHotSpots.exit().remove();
---
+++ scripts/edit.js
@@ -0,0 +1,42 @@
+/* globals jQuery, editHotSpots:true, getCSSRule, updatePreview */
+"use strict";
+
+// Handle collapsing / expanding the config panel
+jQuery("#collapseBar").on("click", function (event) {
+    var panel = jQuery("#configPanel"),
+        collapsed = panel.attr('class');
+    if (typeof collapsed !== typeof undefined && collapsed !== false) {
+        panel.removeAttr('class');
+        jQuery("#collapseBar img").attr("src", "images/collapse.png");
+    } else {
+        panel.attr("class", "collapsed");
+        jQuery("#collapseBar img").attr("src", "images/expand.png");
+    }
+});
+
+jQuery("#enableEdits").on("change", function (event) {
+    if (event.target.checked === true) {
+	Shape.SELECTED = -1;
+    } else {
+	Handle.clearAll();
+	Shape.SELECTED = null;
+	jQuery("#pathString").val('');
+    }
+    updatePreview();
+});
+
+jQuery("#pathString").on("change", function (event) {
+    var s;
+    if (Shape.SELECTED !== null && Shape.SELECTED !== -1) {
+	s = Shape.ALL[Shape.SELECTED];
+	s.d = event.target.value;
+	s.setD(s.extractSegments());
+	s.initHandles();
+	jQuery("#HotSpot" + Shape.ALL[Shape.SELECTED].hash).attr({
+	    "d" : event.target.value
+	});
+    }
+})
+
+updatePreview();
+getCSSRule('svg#hotSpots path').style['fill-opacity'] = 0.3;
